779ffab19022c2d130f634a9496ac24ec21fd808
Quammy93 <Quammy93@gmail.com>
2023-12-14 23:33:04 +0000

    rebuilding of airbnb clone

---
+++ models/base_model.py
@@ -0,0 +1,68 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of BaseModel.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Return a string representation of the BaseModel.
+
+        Returns:
+            str: A string representation of the BaseModel.
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Update the `updated_at` attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Convert the BaseModel instance to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the BaseModel instance.
+        """
+        result_dict = self.__dict__.copy()
+        result_dict['__class__'] = self.__class__.__name__
+        result_dict['created_at'] = self.created_at.isoformat()
+        result_dict['updated_at'] = self.updated_at.isoformat()
+        return result_dict
+
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
+
